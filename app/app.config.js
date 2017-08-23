635883271ba71dd3830d1eef840bd7b2abc02a79
maria-alexandra <alexmary18@yahoo.com>
2017-08-23 22:57:06 +0300

    added main stuff

---
+++ app/app.config.js
@@ -0,0 +1,8 @@
+angular.module("app")
+    .config(function ($locationProvider, $rootProvider) {
+        $locationProvider.hashPrefix("#!");
+
+        // $rootProvider.
+        //     when("/")
+        //TODO add routings
+    });
