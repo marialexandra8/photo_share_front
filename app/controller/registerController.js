759ac9dd9f13f2072243eb7c716b29f56712de08
maria-alexandra <alexmary18@yahoo.com>
2017-08-26 15:46:15 +0300

    register complete

--- app/controller/registerController.js
+++ app/controller/registerController.js
@@ -1,4 +1,16 @@
 angular.module("app")
-    .controller("RegisterController", ["$scope", "HttpService", function ($scope, HttpService) {
+    .controller("RegisterController", ["$scope", "$filter", "$window", "HttpService", function ($scope, $filter, $window, HttpService) {
         $scope.user = {};
+        $scope.register = function () {
+            var dateString = $filter('date')($scope.user.birthday, "dd-MM-yyyy");
+            $scope.user.birthday = new Date(dateString);
+            console.log($scope.user.birthday);
+            HttpService.post("/api/register", $scope.user)
+                .then(function succesCallback(response) {
+                    console.log("succes" + response.data.email);
+                    $window.location.href = "#!/login";
+                }, function errorCallback(response) {
+                    console.log("error" + response.status);
+                });
+        };
     }]);
--- app/env.js
+++ app/env.js
@@ -3,6 +3,6 @@
 
 
     //API url
-    window.__env.apiUrl = "http://localhost:8080/photo_share";
+    window.__env.apiUrl = "http://192.168.0.107:8080/photo_share";
 
 }(this));
