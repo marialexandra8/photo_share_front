de4eead0ab8571500ac84b690fa9d0d5849f2b79
maria-alexandra <alexmary18@yahoo.com>
2017-08-26 14:55:23 +0300

    more register functionality

--- app/controller/navBarController.js
+++ app/controller/navBarController.js
@@ -1,5 +1,4 @@
 angular.module("app")
     .controller("NavBarController", ["$scope", "HttpService", function ($scope, HttpService) {
-
         }]
     );
--- app/controller/registerController.js
+++ app/controller/registerController.js
@@ -1,4 +1,4 @@
 angular.module("app")
-    .controller("RegisterController", ["$scope", "HttpService", function($scope, HttpService){
-
+    .controller("RegisterController", ["$scope", "HttpService", function ($scope, HttpService) {
+        $scope.user = {};
     }]);
