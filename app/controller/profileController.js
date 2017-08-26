eeaf8c61e3669ed5a295beed866c0d428ff8558b
maria-alexandra <alexmary18@yahoo.com>
2017-08-26 16:58:49 +0300

    added more stuff for profile directive

--- app/controller/profileController.js
+++ app/controller/profileController.js
@@ -1,5 +1,21 @@
 angular.module("app")
-    .controller("ProfileController", ["$scope", "HttpService", function ($scope, HttpService) {
-        $scope.name = "Maria-Alexandra";
-        $scope.ranking = "5";
+    .controller("ProfileController", ["$scope", "HttpService", "AppConfigService", function ($scope, HttpService, ApiConfig) {
+        HttpService.get("/api/profile")
+            .then(function succesCallback(response) {
+                var user = response.data;
+                $scope.name = user.name;
+                console.log(user.logoUrl);
+                if (user.logoUrl === "") {
+                    if (user.gender === "MALE") {
+                        $scope.userLogoUrl = "view/assets/boy.png";
+                    } else {
+                        $scope.userLogoUrl = "view/assets/girl.png";
+                    }
+                } else {
+                    $scope.userLogoUrl = ApiConfig.config().apiUrl + user.logoUrl;
+                    console.log($scope.userLogoUrl);
+                }
+            }, function errorCallback(response) {
+                //TODO error pop-up
+            });
     }]);
