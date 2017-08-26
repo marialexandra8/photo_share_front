d87e758fd5f4da220c0f802675e009485919a57c
maria-alexandra <alexmary18@yahoo.com>
2017-08-26 12:47:15 +0300

    added register

--- app/app.config.js
+++ app/app.config.js
@@ -8,6 +8,10 @@ angular.module("app")
                     templateUrl: "view/homeView.html",
                     controller: "HomeController"
                 })
+                .when ("/register",{
+                    templateUrl: "view/registerView.html",
+                    controller: "RegisterController"
+                })
                 .when("/login", {
                     templateUrl: "view/loginView.html",
                     controller: "LoginController"
---
+++ app/controller/registerController.js
@@ -0,0 +1,4 @@
+angular.module("app")
+    .controller("RegisterController", ["$scope", "HttpService", function($scope, HttpService){
+
+    }]);
