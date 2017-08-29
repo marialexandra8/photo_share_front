b0df2c3e2953a6fff23a8b422b609a6cd440b4ad
maria-alexandra <alexmary18@yahoo.com>
2017-08-29 15:23:53 +0300

    deleted profile controller

--- app/controller/navBarController.js
+++ app/controller/navBarController.js
@@ -1,4 +1,37 @@
 angular.module("app")
-    .controller("NavBarController", ["$scope", "HttpService", function ($scope, HttpService) {
+    .controller("NavBarController", ["$scope", "HttpService", "AppConfigService", "$location", function ($scope, HttpService, AppConfig, $location) {
+            HttpService.get("/api/profile")
+                .then(function succesCallback(response) {
+                    var user = response.data;
+                    $scope.name = user.name;
+                    console.log(user.logoUrl);
+                    if (user.logoUrl === "") {
+                        if (user.gender === "MALE") {
+                            $scope.userLogoUrl = "view/assets/boy.png";
+                        } else {
+                            $scope.userLogoUrl = "view/assets/girl.png";
+                        }
+                    } else {
+                        $scope.userLogoUrl = AppConfig.config().apiUrl + user.logoUrl;
+                        console.log($scope.userLogoUrl);
+                    }
+                }, function errorCallback(response) {
+                    //TODO error pop-up
+                });
+
+            HttpService.get("/api/contests/mine/active")
+                .then(function succesCallback(response) {
+                    $scope.contests = response.data;
+                    angular.forEach($scope.contests, function (value, key) {
+                        $scope.contests[key].logoPath = AppConfig.config().apiUrl + value.logoPath;
+                        console.log($scope.contests);
+                    });
+                }, function errorCallback(response) {
+                    console.log("Error in fetching contests for user" + response.status);
+                });
+
+            $scope.goToContestPage = function (id) {
+                $location.url("/contest/" + id);
+            };
         }]
     );
