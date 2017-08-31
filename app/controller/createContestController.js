7d938d6be1cf65d5fdb1af668a676d1dfc082d1e
maria-alexandra <alexmary18@yahoo.com>
2017-08-31 20:26:27 +0300

    fixed pic upload

--- app/controller/createContestController.js
+++ app/controller/createContestController.js
@@ -2,7 +2,6 @@ angular.module("app")
     .controller("CreateContestController", ["$scope", "HttpService", "$filter", "Upload", "$timeout",
         function ($scope, HttpService, $filter, Upload, $timeout) {
             console.log($scope.uploader);
-            var contestId = -1;
             $scope.contestUploadedOk = false;
             $scope.createContest = function () {
                 $scope.contest.deadline = $filter('date')($scope.contest.deadline, "dd-MM-yyyy");
@@ -16,42 +15,22 @@ angular.module("app")
                             formData: [{contest_id: response.data.id}]
                         });
 
+                        var fd = new FormData();
+                        fd.append("contest_id", response.data.id);
+                        fd.append("logo", file);
+
+                        HttpService.post("/api/contest/logo/upload", fd, "FORM")
+                            .then(function succes(response) {
+                                    console.log("succes");
+                                }, function fail(response) {
+                                    console.log("fail" + status);
+                                }
+                            );
+
                     }, function fail(response) {
                         console.log(response.status);
                     })
             };
-
-            $scope.uploadPic = function (file) {
-                console.log("in uploadPic");
-                var fd = new FormData();
-                fd.append("contest_id", contestId);
-                fd.append("logo", file);
-
-                HttpService.post("/api/contest/logo/upload", fd, "FORM")
-                    .then(function succes(response) {
-                            console.log("succes");
-                        }, function fail(response) {
-                            console.log("fail" + status);
-                        }
-                    );
-            }
-// file.upload = Upload.upload({
-//     url: $filter("ApiBaseUrlFilter")("/api/contest/logo/upload"),
-//     data: {contest_id: contestId, images: file},
-//     header : {}
-// });
-//
-// file.upload.then(function (response) {
-//     $timeout(function () {
-//         file.result = response.data;
-//     });
-// }, function (response) {
-//     if (response.status > 0)
-//         $scope.errorMsg = response.status + ': ' + response.data;
-// }, function (evt) {
-//     // Math.min is to fix IE which reports 200% sometimes
-//     file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-// });
 
         }
     ])
